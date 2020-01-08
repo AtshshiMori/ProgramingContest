b16aa2fdd74828fe015ec962386962d4b3234d9c
AtshshiMori <atsmr.30autb@gmail.com>
2020-01-08 20:22:43 +0900

    [add] test.cpp

---
+++ test.cpp
@@ -0,0 +1,18 @@
+//
+//  test.cpp
+//  c++test
+//
+//  Created by 森敦史 on 2020/01/08.
+//  Copyright © 2020年 森敦史. All rights reserved.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+
+    cout << a << endl;
+    return 0;
+}
